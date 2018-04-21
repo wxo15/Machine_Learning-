32438ba8c9d9d31f7ecf4132b5fc5fe853649fc9
wxo15 <wxo_rocket0123@hotmail.com>
2018-04-21 15:26:51 +0100

    Initial commit

---
+++ p4.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import quandl
+import math
+import numpy as np
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
+#SVM=support vector machine
+
+df = quandl.get('WIKI/GOOGL')
+
+#print(df.head())
+
+df=df[['Adj. Open', 'Adj. High', 'Adj. Low','Adj. Close','Adj. Volume']]
+#print(df.head())
+
+df['HL_PCT']= (df['Adj. High']-df['Adj. Close'])/df['Adj. Close']*100
+df['PCT_change']= (df['Adj. Close']-df['Adj. Open'])/df['Adj. Open']*100
+
+df= df[['Adj. Close', 'HL_PCT','PCT_change', 'Adj. Volume']]
+forecast_col='Adj. Close'
+
+print(forecast_col)
+df.fillna(-99999,inplace=True)
+forecast_out=int(math.ceil(0.01*len(df)))
+
+df['label']=df[forecast_col].shift(-forecast_out)
+df.dropna(inplace=True)
+
+#generally, feature=X, labels=y
+X=np.array(df.drop(['label'],1))
+print(df)
+print('separator')
+print(X)
+
+
